faa8633f9d7a82b5ae6682cb373e5ce1665a40ea
msohaaib <msohaib9685055@gmail.com>
2025-10-27 02:10:34 +0500

    updated

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 connectDB()
   .then(() => {
-    app.listen(3000, "0.0.0.0", () => {
+    app.listen(PORT, () => {
       console.log(`App listening on http://localhost:${PORT}`);
     });
   })
